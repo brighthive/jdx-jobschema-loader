c61ef6cb154667b32ebb5eb7ffbc2482844c88e8
Regina Compton <reginafcompton@gmail.com>
2019-11-25 13:42:29 -0500

    chore: Prettier police

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
 
-import './App.css';
-import Loader from './components/Loader/Loader';
-import Job from './components/Job/Job';
+import "./App.css";
+import Loader from "./components/Loader/Loader";
+import Job from "./components/Job/Job";
 
-class App extends Component {	
-	render() {
-		return (
-			<div className="App">
-				<Navbar bg="light">
-					<Navbar.Brand href="/">JDX JobSchema Loader</Navbar.Brand>
-				</Navbar>
-				<Container className="App">
-					<Route path="/" exact component={Loader} />
-					<Route path="/readable" exact component={Job}/>
-				</Container>
-			</div>
-		);
-	}
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar bg="light">
+          <Navbar.Brand href="/">JDX JobSchema Loader</Navbar.Brand>
+        </Navbar>
+        <Container className="App">
+          <Route path="/" exact component={Loader} />
+          <Route path="/readable" exact component={Job} />
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default App;
